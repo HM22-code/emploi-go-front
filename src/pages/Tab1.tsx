960bff6f3c367d72c0492eb8231fb7adbbf44f67
HM22-code <H.M-22@outlook.fr>
2024-11-07 17:57:53 +0100

    Adding react-map-gl & maplibre-gl to display a map

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,8 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import Map, {Marker} from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 const Tab1: React.FC = () => {
   return (
@@ -16,7 +18,18 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <Map
+          initialViewState={{
+            longitude: 5.04631,
+            latitude: 47.31777,
+            zoom: 18
+          }}
+          maplibreLogo={false}
+          mapStyle={'https://api.maptiler.com/maps/a0db1c96-3ec8-4109-b151-8bf92b1fb916/style.json?key='+import.meta.env.VITE_MAPTILER_KEY}
+        >
+          <Marker longitude={5.04631} latitude={47.31777}>
+          </Marker>
+        </Map>
       </IonContent>
     </IonPage>
   );
