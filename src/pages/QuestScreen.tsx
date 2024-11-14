085c982e5d4892fbd5bd42bd67dc573bf4718b1d
HM22-code <H.M-22@outlook.fr>
2024-11-14 16:23:18 +0100

    Remove unused imports

--- src/pages/QuestScreen.tsx
+++ src/pages/QuestScreen.tsx
@@ -1,6 +1,4 @@
-import { IonContent, IonPage, IonBadge, IonItem, IonLabel, IonProgressBar, IonThumbnail, IonCard, IonCardContent, IonButton, IonIcon } from '@ionic/react';
-import { checkmarkOutline } from 'ionicons/icons';
-import trophy from '../assets/trophy.svg'
+import { IonContent, IonPage, IonBadge, IonItem, IonLabel } from '@ionic/react';
 import './QuestScreen.css';
 import QuestCard from '../components/QuestCard';
 
